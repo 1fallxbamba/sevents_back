1be1dc2b5d6e74be9498518c21032087631e2fbc
Bamba Fall <fbamba99@gmail.com>
2022-01-10 11:37:38 +0000

    Finalized

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const db = require('./core/queries')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({extended: true})
+)
+
+app.get('/', (req, res) => {res.json({info: 'Sevents server running normally'})} );
+
+app.get('/events', db.fetchAllEvents);
+
+app.post('/book', db.bookPlace);
+
+app.listen(port, () => console.log(`Sevents server running normally on port ${port}!`));
---
+++ core/queries.js
@@ -0,0 +1,31 @@
+const client = require('../config/database').client;
+
+const fetchAllEvents = (req, res) => {
+    client.query('SELECT * FROM events WHERE places > 5', (err, result) => {
+        if(err) {
+            res.status(400).json({resultCode: 'ERR', message: err.message});
+        } else {
+            res.status(200).json(result.rows);
+        }
+    })
+};
+
+const bookPlace = (req, res) => {
+
+    const bookingData = req.body;
+
+    client.query(
+        'INSERT INTO tickets(code, event, owner) VALUES($1, $2, $3)',
+        [bookingData.ticketCode, bookingData.event, bookingData.owner], 
+        (err, result) => {
+            if(err) {
+                res.status(400).json({resultCode: 'ERR', message: err.message});
+            } else {
+                res.status(200).json({resultCode: 'PSB', message: 'Place successfully booked'});
+            }
+        });
+
+
+};
+
+module.exports = {fetchAllEvents, bookPlace};
